74a4e5105379e1ed9a3d811fa90acba0749e3699
Shivam Pandey <shivam221pandey@gmail.com>
2024-03-11 19:43:12 +0530

    increment_decrement_operators

---
+++ increment_operator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a = 5;
+    int b = 45;
+    printf("%d\n", a++);
+    printf("%d\n", ++a);
+    printf("%d\n", b--);
+    printf("%d\n", --b);
+
+    return;
+}
