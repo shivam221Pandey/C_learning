52bfdb4e337a0314a200f313b00c7abefa32f551
Shivesh <shiveshpandey1996@gmail.com>
2024-03-13 23:42:04 +0530

    code modified

--- decimal_binary.c
+++ decimal_binary.c
@@ -9,15 +9,18 @@ int main()
     {
         result = (j % 2);
         j = j / 2;
-        arr[i] = result;
-        i++;
+        arr[i] = result; //0,1,2 -> [0,1,1,_,_,_,_,_,_,_]
+        i++; //1, 2, 3
         // printf("%d", result);
     }
-    for(i; i > 0; i--){
-        printf("%d", arr[i]);}
+    for(i-1; i >= 0; i--){
+        printf("%d", arr[i]);
+        }
 
     return 0;
 }
 
 
 // 110
+
+// o/p -> -211
